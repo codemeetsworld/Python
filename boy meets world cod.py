5293e614dbab36b260dd913b8be50b23b9c0e15a
codemeetsworld <45041423+codemeetsworld@users.noreply.github.com>
2018-11-14 10:26:06 -0500

    Add files via upload

---
+++ boy meets world cod.py
@@ -0,0 +1,115 @@
+import webbrowser as wb
+import pyautogui as pg
+name = pg.prompt ("what it your name")
+if name == "seemorebutts":
+     pg.alert ("if wish my name was that")
+elif name == "ryan":
+     pg.alert ("are you still retaning flueds")
+     wb.open ("https://www.comedygigs.co.uk/img-artists/artist-183-1.jpg")
+elif name == "Theo":
+     pg.alert ("u have fake ashagoodslaughters")
+     wb.open ("https://cdn2.omidoo.com/sites/default/files/imagecache/full_width/images/bydate/201501/osgood-schlattersyndrome.jpg")
+elif name == "DJ":
+     pg.alert ("get out of the flow state of mind")
+     wb.open ("https://www.comedygigs.co.uk/img-artists/artist-183-1.jpg")
+elif name == "ness":
+     pg.alert ("stop watching fuller house")
+elif name == "Brendan":
+     pg.alert ("carter hall is better than u at lax")
+     wb.open ("https://www.gannett-cdn.com/-mm-/30cd232f7f91fe6a29052f6253ef69a67fbe79d7/c=0-0-640-361/local/-/media/2018/03/21/Bergen/NorthJersey/636572216898260718-FrischHockey-BenjaminFeintuch.jpg?width=3200&height=1680&fit=crop")
+else:
+     pg.alert ("well thats a boring name")
+
+points = 0
+show = pg.prompt ("What is your favorite TV show? ")
+if show == "secret life of pets" :
+     pg.alert ("OMG LOL xD")
+     points += 600
+elif show == "adventure time":
+     pg.alert ("one of my favorites")
+     wb.open ("https://i.huffpost.com/gen/1120144/original.jpg")
+     points -= 200
+elif show == "fred":
+     pg.alert ("well were practicly twins")
+     wb.open ("https://www.youtube.com/watch?v=cg-z_JkWPrM")
+     points += 500
+elif show == "back at the barnyard":
+     pg.alert ("Jess laweis that was my old favorite")
+     wb.open ("https://images-na.ssl-images-amazon.com/images/I/51j0IiAoewL._SY445_.jpg")
+     points +=100
+elif show == "the fairly oddparents":
+     pg.alert ("I love that one")
+     wb.open ("https://i.huffpost.com/gen/1120144/original.jpg")
+elif show == "blues clues":
+     pg.alert ("I love that one")
+     wb.open("http://www.gstatic.com/tv/thumb/tvbanners/184328/p184328_b_v8_aa.jpg")
+else:
+    pg.alert ("Your favorite show is " + show)
+anamal = pg.prompt ("Do you like silly Kawala bears that double as magicians?")
+if anamal == "yes" :
+     pg.alert ("me and you both")
+     points += 600
+     wb.open("https://www.youtube.com/watch?v=finCHypE6qg")
+elif anamal == "no":
+     pg.alert ("who are you")
+elif anamal == "no":
+     pg.alert ("who are you")
+elif anamal == "perhaps":
+     pg.alert ("who are you")
+elif anamal == "I could tell you but I would have to cut your head off":
+     pg.alert ("ok funny guy")
+elif anamal == "maybe":
+     pg.alert ("who are you")
+elif anamal == "yes no maybe so":
+     pg.alert ("who are you")
+
+else:
+    pg.alert ("Your favorite anamal is " + anamal)
+    wb.open("https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/292348_435844276459770_595946018_n.jpg?_nc_cat=111&_nc_ht=scontent-lga3-1.xx&oh=81c040841d95e0d7de334aa3b24ff608&oe=5C5E321E")
+    
+beans = pg.prompt ("What type of bean do you like? ")
+if beans == "the rice and beans cronicals of ishia dong" :
+    pg.alert ("the rain will fall but the sun will shine again")
+    points += 300
+elif beans == "lima":
+     pg.alert ("I see would'nt be my first choice")
+     wb.open  ("https://i.ytimg.com/vi/AMPogPrKQeE/hqdefault.jpg")
+elif beans == "fava":
+     pg.alert ("way to dry")
+     wb.open  ("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNBntpsFQjjYlSlf95O6lms0-pmvk5nawpnBYgvRHCjGPvRgg1TQ")
+elif beans == "Kidney beans":
+     pg.alert ("I see a man of culture")
+     wb.open  ("https://nouvelleviehaiti.org/wp-content/uploads/2017/07/funny-llamas-pictures.png")
+elif beans == "coco":
+"https://nouvelleviehaiti.orggfAlL3k7DQQlCSS2NyF1JJIHAp0piUpGfKSYCuoByS4VwujVAOCruOcap4WnnqTyAAmTy5D1WP8UfxKZSJp4UNqOFjUdJYD/xAMv7yB3XnXGPFGJxH9Wq5wmQ2zWj0aAEaLb0bH/xDzUSKdOKptrYDmD6KpreP8SGMDcriWw8xcGSJkQBaCvPRjHf7j8Z+qlGMPTr/hLVJqR4sxTRAquAvEHNrrfVE4X+I+JpkAxUg3DgJPSRosTVxBiN9oUTapsZFlOi03bf4jYo1A/ywCf9OCGkHSYuYU3/AM8xUmXsAdNi0GJ2vssEK87+qfTJmcpO8lTdnpY4mmHk6gk6jS99OSDcHUtRJhFUMQ430+CkFQSHAzGm/wAlHtYFXQxJcTc306I+u9pFnSea5UeyCIF+yBpVoscsCLJW7B7WmTKkZXM+9Ym9vso8VVk2v6Jn8yWkH7fJOchYDGMBBa1oIA0ET0shf5Wi6zjEEgQbyTNx0lCOxAJsBfmn0GS4NmJiDqJ20TksGjuJs8oDYlusb7SfkuOoPaxr48rjl2uYzWjoVPXr5hlkidRa8W7zrqu8I4ax7nNe9wOQllrZhFidBabp7+APWrka+Xy5TltmEm7uphV/tcsxY8uSKFQ7g9kHVp6uAgb9uacvIdq4omJaJXf5gSCNTrKAfMwUTSoyYkRMTNu88lZrKi9pGketh1WgZxsVKTKDoaG6OE+Vp6d1jG4mCWum9vRGUaxkBt5HIlZ2a5JoeHElxE6EEuOu9hBiEe2oHCUFhKTQ0OgzrPyspnYekdSbCLMLhz1HdcuWWOVKvdF1NSXpKOXU0JyQJJdXJSMgEnuAEkwBqTZVnF/EGHw39R4k6Nbd1ug0WI8Q+PmVaJpMo3e0g57x2A3StLbnib+IdQVDTw8NaJBeYcSQdQdAFiuNeI69Yeeq50gTe0C2gshjROrjbp+aKqxb5ce5/RLHmloxz03MmZk1zlps0wcm+0UQcjOGcPdWqNYAYO/Ic0tnIdhGuecrWlxOkCSrqr4RxIpmp5eZYCS8D4QT0BW64Fw1tNgYxuUbncxu7mrluBdpssrk6MfHjrmvD2ggwAfhf8so3VXOsJXsP/xCnmLoE3j4n7GIVNxD+H5dVaabsjf7iNdtOafsw9edMRgKHs/eOospc41j0C9TwfhihTblNNp/7DM49yVl/GPABSa6swQ0WcBt2+kdllZs8vHr6xdT3tIHLdR1HaCLDRDPxsGw/wDL9B+qiGM5j5lX61GlhSbkaZNyLmOaGL+d+nNMdiZvH7KWlWMWF1FllCGm2NQbfRTsrj4fVD1ah1IN1K6gA0Em5m2l1YTMYX3aQNMxcQPXrorDiOBNFvng5wDTLXtLbamBcWjVVBcA2Bf9VO6ochaRAsW7bx66pUEytHms4TcExBvFtdlDig2C5twQSZjym9kOH9/3UlB7HAhzZnkTbaY3/ZIlew6FT6gRsdv0UdbKHEC7diiuF4ulSqZ3AkgjKNhzN+QV5XjgxI4Y91ix/eI+XJWeHw3sGtzgB2p9bD4BFYXiArMzRAkiDqYOvZQYjEsBNpPL6TOi5Ms7nxZpO1zSxYazyxLhE/myDOOixcR20hVH884ty6XsJ35kclKMfl8rgHEbxPZZTxaN9FgLsJSlK9QyUOPxQpU31CCQ0EwIkx3WI/iJ4lq0HNo0nBsiXEHz3tEf291gcZ4lxJDg6rUIdAIc4wYS2W298Q+OX03s9g5jmva18EXaDMtN9Z7LOYjxriTX9sDk8paGEksE7wfT4LICo4i88xI+ajrYi1weSm2hZ4nElzpJLnkyTvJMkqFzsoJi83zbBD0cRDDHxj7od+IDgQ4ztoo5tCerjwRB1G4G3JVdc+Y90mxe8ck2uLAzJ3WmOocRly5KaFc8I4FUqn3THPVVaZmA4DXqOaBTdBNzGnX916j4a8N+yaM8F0a8uiK8O4D2TAIVwSscs144paVBrVKHBBmr1snU6oAuVHsrQoPXfaoRuKYoq+KMWRchILqPuoMXSbUYab9HBCUcVmMclKwzulP0MH4l8BgU2twzS4iS5xu42/IC83xWHeww4EEbGy+iA8gc1nvGHAmYqi6ABUaCWmBqLxMdFrM02PFGVYVhSxRme3YQq/E4ZzHZXC/LeF1gmxJF1dm0CK2IdN0S1gLfekmSANAg2V4bEA31UntJv7oHLfoopHDDumPW6kr4hzjmyxENJAtbRQVq28ruHxJ9wnyk/guj4Haz5i6T2gAu2iDbordnCQWwBmn/ANLTbrZFsmmyCQdoiw/OZWN8s+EoMPw6q9umVp3O/oTMIStw6o0uEC0X6cwr91UglxcALiAocHxBodGUEX1vEbomeXfwbVTeJva002NgyJO8CwEbCblOfUJiZJJuDuZhWtbiOVzmgBrXamIn11CgzAuLjfLBBmPXr6IuX/A7W4dlu4melgPVCVsU/MchMdPzVWGOr+1YcrtW6DmOaFbhJa3M4MsLX05nqo3v+ifT5QXGMeKFJ1QgkNGgIB+aOhUXjLhLsRh3Na4iPMWgAl8XAvou5TyDiuI/m64cXZXOAzFxmSBqYECwVLX943lt/X0VxxbBPwjwKrS0uZIDgCQDInuqEVWzeTYwNO14UkTsTOunIdNlyre4Gug1npCgfV2ASl2okRve3Uo0Yt7nRDnRGw+iEqvdMTZT1XXABmdTeD1Q9RsEmdOamAi+RliOqZQbJMwI9VNSqw3QEnT8Kfh8kEuieWnwRsFguHvc4Fgkfn6r2DhOF9nSbmYM0Cw5+qzng/hzXAVM1hENgW0OvJa2vUIWeWTXGFUx0WFlT8c8StoASZcdGiL9SdgnY2tAJ5Akx0uvLsfjjVcXbuPwHL4KMMbleV26i/xnjPEPJyuaOjW5j8XSlR8W4hvvHMNw5uX/ANm/dZ9j400TjiFv6RHsvMV4vrOP+mMschmPxNh8EPQ8ZYhhBf5hvIj5tVI7FQIHyUDq0hHrC9nqnBPE1OszODeYLTqO8bdVdMx7XRB15bLxPh2ONGoHA+X+4DcHX86LR1/E1IA5HvmDls7WFjcLLwuWVceJfF721DRoOjLZz9STuGzoOqz1LxBi2nM2tUnq7MP/ABdIVBh8VqTuiRidlpMP1V8sk1IL4lxnE1XZicv/AFAZ8h1VeeJOPlrAPbz/ALx2PPopnV5VbX1MiFp0xt2sKNJoOV2mrXjcG4P7IbF0C3Q5hzH51UWCdMCbzbsrXC5pAaJiD8P8LO8VFUpKMwABcM0yNLx29FYY7DGQHs82veeyOocP9mA5jWudMnciR7on4+iVy4CWrUcGtLWwByv63VbjMYTcx5hIjbb7SncSxJjU9AOltPiqlz3OPaf3WOGP2k7UxBDjoQdd+qjfiBqBHayZUyybGITfLBn/AHQPhqt9Qy/mjuZB53hOdVDhaB0uhWOvCdTgXAsNU9BfcPxboDTEdIAB6oWtUOY+cdJupOGYlpID7diBzmSdkTWo0iSQ3/ycJ+ayt1eYH01mVV4qx/scLVeCQcsNy2Mmwj4qxlMxLKZaDUDcrSHS6IBGhvoV1UPA/ENerUe1r8zi1rR5nF5E3tNxros8HgG4m+i9P434qd7SoKTG06fRrS9xzEkz6rzjFNbmdvvP7KJlLSD0hmfA3Pf06qXEVi4mZBLnSJht4sB6K78OVGVKtNtTKDTafZj2eYVH3c0P+MT0CA8QUBSeNM1RmZ7S2BTc4zlbfUKjCUaMNJIMEABwNmnWCN5Cd7AEF94g5dLuB+iI4Li6TGubXovexxafK4tggmTGhMGOi7XxE0G0WUomq57TbO4HQGBNh15pWABhsE6oQxo809FrOB+AajyC91PLN4dJ+QInuVBwjgVek5pcxzXHYWM9rg9jC9T4RTexvnJd3yj5BTVSIG4anQaKbBAA13PUoPF18qfxeoZJBlZDiGOqvc9rQdYzEloaALxvqfVZTm8tPnDniTjQbTLQJNQOaI7R9157QdBA6n6LZ4XDspmffcIAmTEcgdFluOYcsqkj3XHMOnMLSZzqC4/aifVUFSomGryTCVaCzLtRybkXCEE41pMAakgDuSr3GcAyUy6XGBrCK8EcBNaqKjwfZsPo53LqvXcFQbEQI7LPK8rxunz0x0WUrXSvTf4g+BmZDicM2HNvUptHvN1Lmgf3DWN+68tYrlTRDaigr3MpNaU6rRI7JkL8O0wa94gMJMieWy1lCsJiBmI8xiLAagLLcGrCnLiPM6I6ATp+bIitjiSY1vJ5rLLmpq+OMZFhrY89NSSo3CYy73v81UYLEBsucMxnTkjX44iLATETaN1lnbLwTmLwDTvr8up5Kp4iL+TaBbtCs8U4lnle2dSdOoBQbME6S4uMc8pO/wCiWFtuzVAYTOaAAJv6C3M3UWQq34/TIrPD5mRqMkiLODdgRFlXDN/aF0AMXwbDRGYKkCTci353UBoO8xA09717pU67iIj4Jha0MMMwhwN9b/BTHB0zdznE/wDEW+aEwFEk7nmCYU1ehTJ94t5giTPdZh9RgBYn+IfE2+zFNr9ZkNMgxzja611R5gxrC8g8Q4h76mRrS58m+ruWWwiOi1z6G1Tw8ONQeUEnNAOhMaFQ/wAkXuIyCXEiGgWgla7hjMO3DhuJ9pTqX53g/wBvWCoOCu9o+u8VKQFM5aciHuHvZiwm/JKYwgXCuFU2+Wsw5SM2UOLHS0lpAgHNtqQFn/GFf2td7w4+zFmSIgDaBvO62tXhrXYU13VHMkEQQIJGkidzPxWJw2AdVcAxhcJvqfSRoi3Rg+GYepVeBTk7G0i4g2+/Rem+FPDNbDhpdVAtpAJiZi7Z16hc8L+Hv5ce0qC/9rZmO+yscfxIjQo9lTEcaFFhLsuZxMkm8nmon13P6AbBVdEucdVPVxUeRl+Z5lTtegXE2PeHNabkQOV1m+I4cgBs3Bv1vJv3PyWtc0sEwTO+6z/EjmJAssM8vjTGBqmFLmS3y/X4rP4ngpcTmctZhHxT8x1/JQOIglVLoMBxDAupOh4kbOGh/dCAL0Kvgw5hDgCFn8Z4cIl1J0f8XT8itMfJPqMsVEAp8NhnVHBjNSb9OpVnhfDlQ/1HNaP+N3fstdwThrKcBoj6nueaq5fha0t/DPCvZtYwe60es7la2lTa1UwxQos6rOcU8YBpIlLiDW28zDZ3xXn3i3+HrHudVwzgx5uaceUk6lvLt9FzBeJi64JVni+MGxBR7D1rybEYd9Jxa9pDhqCiOF4Q1n+a1Me8Zif+IOxPNa/xlRZXoivlGdhDSeYOkrIU6zgAW2IsRoOxHVO5J6WHGaAkezDSD/8AWbHlDTztprb4U5aCZEjmDqPXfTojqmIFZvJ8+hOkTtMW6hAVnki+unX16oiXKL8pnUjbmrGtUzMLuYPTe6r6dDd1p339Eyo9wsT5QIHYaBTZKSek4E3O0c7DoiG8WIMZjaA3rff82TaJbkzdPiSgn0pBcAYGv2SgPxbi9wJN9yb+nZMqV4ZlETv8RYJtGpYgiDaFFWZv17zzVA/2pc10kfqoaLTz8s3K7kAaSmtnLKYHteYzazrGoGkpVMW22ZpcY1E/kqDC0HkiDlmLnr3TMh/3gdAlqB9WZlRYzw9Tc/MwBt5MaudrqdPRW73gAnldYjjXjZrKjCzPkafPoM3SCJELa2TsjeK4c0CKlem0sv5ySQy25Puiw9U3wzwV7qDHPpUi2ofaSc2YB5nnss54h8XVq7alLMRSfIiATlO0q98K+N2zToVtIDQ/kdACBtCmZY7DUu4DTZSqBjM+YXa8yHHUT8Sq7gOBZRZlDYdebCdVY+I8NiHNHsagY0SX7Ei2hi26p8FjwDkJhxO51PxSzqsRHF8Q7QclW4ehJzHVF4zEsaYeRKhfiWEQCOSxbSm1nn3WmJt166I7h2DDTrmdsNlWNxdNhkubyEn8lWOKxAY0kXMa7qMstHJtBxOS4hUVdgmN1bOcS2eaGOFk6rPW6ucKKrm3Pl/OSJbRACmxQDbCDKAL3D7KiGCnI6Jpw9p6yicOJFxv8pMfFT4mjAA/JV6TVfTw4KusNhoAQFBhzAfgVmyp5oGwT6KqzjeCLpOZw7EfosbjfDj3Sc5PQ2W3xNVzzH0TTRaBefr9UW88KnTBYKiaLofIGxP0lWrcTNpnlG6u8ThWOBloI9PnpCouH0KdLEBwFiNNRM7JybCavw6s7DVABJJzOG5A/wBvM9FiXViJHa+mm0L2WlDugIItG/f6Ly7jnDiyo4AgkHWzT8CVWLLJUUn30nYhTFznuHTU8+qHL8puPsVOastnvA5qqzROfJ1nkpaVKXXsBfvdC0zcwJESpqeJMRqNidQkFy1uUZxBEQIAJB5keiTOIB2YlgJkS42A6cpVVh6xNrzJJ5QnveMpJeOYaRvOphZ3H5QjqUml1zY6dOQXMg0DiApGuqB2dzREaeiiqYjMYMDWTz6q4HSaUASTO/ySGKDSRlBtAnbrZMqYcCxtJM+n+VwuAJbqJHT6phG6vmNhewmfsrbAU8PkHtC7NvAn7qPMxlmsvr1vvBsR2QVfEhxmG+ghMPofEcWjB+1a+/swc2WSTFzC8axeLJe5zhJmSTueZCOGPc1gZqBYgnbsqquxzzIAj5qLn7UaR1sQS4XiRqrjwvhzUrsD7AkeYST3EdVQPwj3OIjQC+19EbhDVpQ9p8zbgtuQnuQPoGpTmmWSbtiRrpr3XkvGKoovLqFR5gmJEHTta9p6JYDx9igAx5Hctv8ANUmJ4m/O/MY9oIcYiQryzlAnA1HVqhzvJIImBMz1m1t0/G1y2ofY3AaJvIB5D4SgeD1GuqPYfK14a0kxbkZIsLfNO4dialGq8sh0BwJgFpabLO5SDdcoYs+2pPuSKjRBFhcTA12C9H4i7K2YvP5KxnhykHYimYm5MWAG8+i2WLGaRy1WGd3038YdmI8gBMuifjsmirAICFf5DJcNIHPuhBVcZI+f1SlaUTlknNHYa/FAcRrNbMdPSFIwFslxA7a/5QOIaTJjXnEpgbw7HgkA8vpzRDcVJgnf1WewYIf3/VXlCleTuVUpWLnDMkjl90VVygGOWyFpVIEDkoMTUd7zfUK/aI05Ts2UJXxMd1FiMdsRH0VdXxTRq70mPig0mJxDnQL/AEhBYggVB0G28/ooK3E2izb9VXYfGPzkuGqZtnw/FwW2HwhQeKuGNc/OHsYSLl37kj5BDcOxdw4i08lV+KOPuc4hhsLRt2IVY1nkoOIYYglpqhxHN2WPSNPVCYdxEyQOxnXrKjxFZ7hE72AkD4HZQUwZ81xNwqZ0dQwpHmBmdAJ7JewB3j7lIYotaQBEzb1sgatZxABNhopmySuEE3kn8lRh3myx1KTXSLkhPoNGaRLtlQWFKp5CWzGrp26dUG4su7W8TcAenok7GOjIwWgg2+PqocgIJE21UyGnq4pxfnJJzCTJvy+yY97T8z+ygG3ZMq69FWjddWG/yUJJ5psLsKoTZOdIB36qfDU7Ek/vzUDXRc6cuSeGl1otFhpquO8Fo+tSaHEgw0wQLmPUrlOo1pnl6qJ9Ue6SeVlC1oB/VPVvI0ne4GCT17o2u5pAEAzzVUcUwmLfBSMxYaR+iLKNDG4QSSTEgTA2CcKwAAGn5coRzyXa2TXVcumsKJjcuxpfcGxWSq3JcgwQLai63TwA089SvM/DOIc7E02gTLhK9I4ja09VVlnbXxqXiNGfje6hxRIaGjcyeXRPrV7EHcgD9UIQ4kg6D/MJVtCrVAJJ/wAAIA1HOJ0jrZLGVjOVoudf0TnBwbsElHYZhnRXTW2Cp8IJi+91b04tOiqJojMUylUvEWP1UNXF/OwQ9KobmdvuE9kJxeGa7ZUOL4XdWzcTa9lHVqSqlJnqmAI01UuG4cSRJVo8BdphP2Kh+Mj2dIBhAdrB0cOSzDpqy17Y3BgSD33CueMvqPcAPdG88vsqPEVajDcCNecTKXtL0xyquxcl0nUWmw07IYg/dEYt0uvY9lELO0W86I8vgB0d/wBVHWafeiAdOqPGaq3KIBm/5yVbXa4HKSfLaFMvIdhtgJ6yozUIJAteOyTDzThThs/5KsCMPLfT8nunV36REn3vRLCVBlO50A+6Z7MB0Azz9e6n6aCro3/r9yoXOsOe6fiDJgbQFw0LbzKoJqLA7MbDkNAFFHUInhzS4loZmtEffkj8DwwPYC4wdLdPupuWiaajw5psHZtI+O5UNRzaRAMa2DhBA3v1VXQx7m7+m35dTYrE56cOInYn6d1xf48980x80QczmHXY/bkuVn0mwQ3Nm92DJHUys/kI0N0UyoQ0DbU7x+iq+L8pbWNXhVIeYSZF5I11UOJoU2kBzSAbggzCGxFcupi5gHmYCFqlziBOh/dLHHL7QtGMnfKzSbXTa9CmREkHmY/JVdXqkgDWPVCtzkxr0haet/Q9A/hzgAKr6huWtsdr2WsxTczieSxngEuNYNDSQG+YyYFteS3FRgBJ6GFeU21wqqrYVrTJuTft0CFxNVoEWlN4liCQCDBuD+eqAyZmwZ0vssmpjWgTOu3c/nyUGNrxb8hdNSHQ7RotyKDfJOY7mPQJHROHr5W3RbMYDp2Ve6h5R0CGpMNieaolxVq/JDU8WWnpp6ISvVhQtq5khpcuve3xUb6oG6GCY6NEASK8kRonYnFFrdYvryhBUXQ0uOgBJQHGsW1wblMxPY7hFRndQyvjRcm4ntcidOSGfi6cD/KC9sSIiT6fgshRSknn+bp4+KfWCyxI9oJAGovuoa+ALSQA85QZlgbbMGyPMZuR8UdwSm1zSHOvYxA20vqivY02Al4ku11giQ6O2YAonlmP+oCYPD1GeVrYNxq0OJDsh3uMyrsVg6jiXESXEQQQQS4kbGNWnsrYY6Tms0gk6EyC8vNp5oJvESMzQD7pa3TRxzEnqOi0wstCubgy5hcP7XGewaXkz2Cm/k3ZCct9dRJ8oeYEyfK4FJmMIA92LyIPmluS8HWOUaynjiJkWbvGsXY2nz/2sHqtaDBw5zKrmybTYAF7gHZZDCRbfX4ompgWgOc0ut/3mXDb4oXHY41Pea1xJJgg7nNEggiJMX3T6HGn+6Y0jTS7SIvtkEdkXnkA8XhXNPmAEkgQQbiJFt7i3VH4bDETnaMzcpBzNgZXCQTMA7QUJxLGOe4ExYuIMusTBm51t26IunxC5lrII0IMEzMkzOvpcoy6Cfh4LKlQuaAHHNGZoI8zmyQTa9u65hcSxoLXNdLXOFp2J5boT/8ApHO9zgDmbHWM5qfGSpn8TzEusJJJAG5Mn6rLLHnoj6Wh/N1xunqkknTcO/b7p39vwXEkXsC8Hv3V5T90JJLlz/oKGl/Wd3KJOq4ktqTX/wAPf7/zYrW4vX0SSV/GuDL4rX/9BR09D3H3SSWLaqzFfdKtoEkkKdq+6ex+iEqaBJJCQ9fdRUd/RdSTUMp6Huo3e96JJIS7W/ou/wCp+ip639FqSSIx8naubql/aeySS3rMRwnU9lYcT91v5sEklyZ/2auwvvHsUK73j3+ySS6Me0h9yujbukkt6EZ1Pqod/RJJE6NPiE0pJIodr7f9Qo2rqSYr/9k=")
+elif beans == "Bush Beans":
+     pg.alert ("I see a man of excisite taste")
+     wb.open  ("https://i.ytimg.com/vi/u3meNrZXfqc/hqdefault.jpg")
+else:
+    pg.alert ("Your favorite beans are" + beans + "who is dis mans")
+    wb.open("https://mk0mexicanfoodjovj3d.kinstacdn.com/wp-content/uploads/2017/11/Mashing-Refried-Beans-in-Pan.jpg")
+
+lizard = pg.prompt ("by any chance do you have a pet anxiety relieving lizard? ")
+if lizard == "yes":
+     pg.alert ("were practicly twins")
+     points += 1000
+     wb.open("https://www.youtube.com/watch?v=ewVG3cEG5K0")
+elif lizard == "no":
+     pg.alert ("this coding thing is extreamly tiring")
+     wb.open("https://www.youtube.com/watch?v=7fE0YhEFvx4")
+elif lizard == "perhaps":
+     pg.alert ("this coding thing is extreamly tiring")
+elif lizard == "not at all":
+     pg.alert ("well ur boring")
+elif lizard == "not at all":
+     pg.alert ("well ur boring")
+elif lizard == "yes I love lizards":
+     pg.alert ("well me and you are practicly twins")
+else :
+     pg.alert ("well call me sheral and wet my wistle")
+     wb.open("https://themighty.com/wp-content/uploads/2016/05/x20160520_140124-640x213.jpg,qv=1463784177.pagespeed.ic.9ePh95j_Rf.webp")
+
+
+pg.alert ("here is your score")
+pg.alert (points)
+wb.open ("https://www.youtube.com/watch?v=LEtorWba_Fg")
